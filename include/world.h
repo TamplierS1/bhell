a6e8d9ffc946f299abfd7f09f8c824499680124f
Azamat Nabiullin <azamat.nabiullin.97@mail.ru>
2022-09-26 08:45:08 +0300

    Player now constantly shoots bullets. (#2)
    - Now bullets spawn in front of the player at a set interval. They are
      deleted once they leave the screen.
    - Added the viewport variable as a member to `World`.
    - Added an error message when actor's texture could not be found.

--- include/world.h
+++ include/world.h
@@ -14,15 +14,24 @@ public:
     void run(SDL_Renderer* renderer, SDL_Rect viewport);
 
 private:
-    void render_world(SDL_Renderer* renderer, SDL_Rect viewport);
+    void render_world(SDL_Renderer* renderer);
     void render(SDL_Renderer* renderer, const Actor& actor);
 
     void handle_events();
-    void handle_input(SDL_Rect viewport);
+    void handle_input();
+
+    void spawn_bullets();
+    void spawn_bullet(const Actor& shooter);
+    void despawn_bullets();
+    void move_bullets();
+
+    bool is_out_of_bounds(const Actor& actor);
 
     std::vector<Actor> actors;
+    std::vector<Actor> bullets;
     Actor player;
 
     Clock clock;
     bool running = true;
+    SDL_Rect viewport;
 };
